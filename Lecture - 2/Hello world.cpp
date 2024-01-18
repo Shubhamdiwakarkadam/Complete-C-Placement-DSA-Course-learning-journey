a5a25f8cacab4587cb698d1ef2c77b73391293a3
Shubham Kadam <shubhamdiwakarkadam@gmail.com>
2024-01-18 21:58:25 +0530

    new

--- Lecture - 2/Hello world.cpp
+++ Lecture - 2/Hello world.cpp
@@ -4,5 +4,86 @@
 using namespace std;
 
 int main() {
+
+    /* data types */
     cout << "namste duniya:-)" <<endl;
+    cout << "shubham \n";
+
+    int a = 5;
+    char ch = 'S';
+
+    cout <<  a << '\n';
+    cout << ch << endl;
+
+    bool d = true ;
+    cout << d << endl;
+
+    double e = 1.25 ;
+    cout << e << endl;
+
+    int size = sizeof(a);
+    cout << "size of a is: " << size << endl;
+
+
+
+/*type casting*/
+    int g = 'a';
+    cout<< g << endl;
+
+
+    char cd = 98 ;
+    cout << cd << endl ;
+
+
+
+    char ch1  = 123456;
+    cout << ch1 << endl;
+
+
+    /*how nigative numbers are store */
+
+    int j = -5;
+    cout << j << endl;
+
+
+    /*Default*/
+
+
+    unsigned int k = -12;
+    cout << k << endl;
+
+
+    /*operators*/
+
+    /*
+    +
+    -
+    *
+    /
+    */
+
+
+   int l = 2/5;
+   cout << l << endl;
+
+   /*Relational operators
+   
+   =
+   <
+   >
+   <=
+   =>
+   */
+
+  int o = 2;
+  int z = 3;
+
+  bool first = (o==z);
+  cout << first << endl;
+
+
+
+
+
+
 }
