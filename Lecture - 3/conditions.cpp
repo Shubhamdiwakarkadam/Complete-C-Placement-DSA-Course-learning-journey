57bad3c613025dda008870ed17cf0639e7f92c98
Shubham Kadam <shubhamdiwakarkadam@gmail.com>
2024-01-19 20:53:42 +0530

    lecture 3

---
+++ Lecture - 3/conditions.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   // int n;
+   // cin >> n;
+
+
+    //cout << "value of n is : " << n << endl;
+
+    //if a is positive
+    /*if (n > 0){
+
+        cout << "A is positive" << endl;
+    }
+    else{
+        cout << "A is nigative" << endl;
+    }
+    */
+
+   /*int a,b;
+
+   cout << "Enter the value of a " << endl;
+   cin >> a;
+   cout << "Enter the value of b " << endl;
+   cin >> b;
+
+   if (a>b){
+    cout << "A is grater " << endl;
+   }
+
+   if(b>a){
+    cout << "B is grater " << endl;
+   }
+   */
+
+
+
+  /*-----------------------positive nigative--------------------------------*/
+    /*if else*/
+
+   /* int a;
+    cout << "Enter the value of a" << endl;
+    cin >> a ;
+
+    if(a>0){
+        cout << "A is positive";
+    }
+    else{
+
+         if(a<0){
+            cout << "A is nigative";
+        }
+        else{
+            cout << "A is 0";
+        }
+
+
+    }*/
+
+
+
+    /*-------------if else if----------*/
+
+
+    /*int a;
+    cout << "Enter the value of a" << endl;
+    cin >> a ;
+
+
+    if(a < 0){
+        cout << "A is Nigative" << endl;
+    }
+
+    else if(a > 0){
+        cout << "A is Positive" << endl ;
+
+    }
+
+    else{
+        cout << "A is zero" << endl ;
+    }*/
+
+
+
+
+
+
+}
---
+++ Lecture - 3/homework3-L3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 24;
+
+    if (a > 20){
+        cout  << "shubh";
+    }
+    else if(a == 24){
+        cout << "shubham";
+    }
+    else {
+        cout << "Kadam";
+    }
+    cout << a;
+    
+}
